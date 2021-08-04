65a86cd253275b06c3039548671e33ed1dd0a4bd
Kedasha Kerr <kedashakerr@northwesternmutual.com>
2021-08-04 11:57:44 -0500

    working

--- src/demonstration_03.py
+++ src/demonstration_03.py
@@ -7,7 +7,12 @@ Examples:
 - string_int("6") ➞ 6
 - string_int("1000") ➞ 1000
 - string_int("12") ➞ 12
+
+to check the type of a data, use type()
+convert int to string with str()
 """
 def string_int(txt):
     # Your code here
+    print(int(txt))
 
+string_int("6")
